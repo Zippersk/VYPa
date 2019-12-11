50f7797d8d8eae21ec7afb4b46ab14cc2adfb133
Matus <mucka@netsearch.cz>
2019-12-12 00:06:19 +0100

    fix build in functions calling

--- src/VYPcode/Functions/buildInFunctions.py
+++ src/VYPcode/Functions/buildInFunctions.py
@@ -1,15 +1,15 @@
 from src.VYPcode.AST.AbstractSyntaxTree import AST
+from src.VYPcode.AST.blocks.base import AST_block
+from src.VYPcode.AST.blocks.declaration import AST_declaration
 from src.VYPcode.AST.blocks.function import AST_function
 from src.VYPcode.AST.blocks.function_return import AST_return
 from src.VYPcode.AST.blocks.value import AST_value
 from src.VYPcode.AST.blocks.variable import AST_variable
 from src.VYPcode.Registers.Registers import VYPaRegister
-from src.VYPcode.Instructions.Instructions import WRITEI, WRITES, READI, GETSIZE, READS, LABEL
-from src.VYPcode.Stack import Stack
+from src.VYPcode.Instructions.Instructions import WRITEI, WRITES, READI, GETSIZE, READS, LABEL, DUMPSTACK, DUMPREGS
 from src.VYPcode.Types.VYPaInt import VYPaInt
 from src.VYPcode.Types.VYPaString import VYPaString
 from src.VYPcode.Types.VYPaVoid import VYPaVoid
-from src.instructionsTape import InstructionTape
 
 
 class VYPaBuildInFunctionClass(AST_function):
@@ -19,44 +19,50 @@ class VYPaBuildInFunctionClass(AST_function):
         self.function = AST.get_root().add_function(self)
         self.label = f"buildIn_{name}"
 
-    def get_instructions(self, parent):
-        self.parent = parent
-        if self.return_expression:
-            ret_value = AST_return(self.return_expression)
-            ret_value.set_parent(self.function)
-            return self.instruction_tape.merge(ret_value.get_instructions(self))
-        else:
-            return self.instruction_tape
 
 
 class PrintIntVYPa(VYPaBuildInFunctionClass):
     def __init__(self):
         super().__init__(VYPaVoid(), "printInt", [AST_variable(VYPaInt(), "number")])
-        self.instruction_tape.add(WRITEI(self.function.stack.top()))
+        block = AST_block()
+        block.add_instruction(WRITEI(self.function.stack.top()))
+        self.add_block(block)
 
 
 class PrintStringVYPa(VYPaBuildInFunctionClass):
     def __init__(self):
         super().__init__(VYPaVoid(), "printString", [AST_variable(VYPaString(), "string")])
-        self.instruction_tape.add(WRITES(self.function.stack.top()))
+        block = AST_block()
+        block.add_instruction(DUMPSTACK())
+        block.add_instruction(DUMPREGS())
+        block.add_instruction(WRITES(self.function.stack.top()))
+        self.add_block(block)
 
 
 class ReadIntVYPa(VYPaBuildInFunctionClass):
     def __init__(self):
         super().__init__(VYPaInt(), "readInt", [])
-        self.function.add_variable(AST_variable(VYPaInt(), "number"))
-        self.return_expression = AST_value(VYPaInt(), self.stack.top())
-        self.instruction_tape.add(READI(VYPaRegister.DestinationReg))
-        self.stack.set(VYPaRegister.DestinationReg)
+        self.add_block(AST_declaration(VYPaInt(), ["number"]))
+
+        readi_block = AST_block()
+        readi_block.add_instruction(READI(VYPaRegister.DestinationReg))
+        readi_block.stack.set(VYPaRegister.DestinationReg)
+        self.add_block(readi_block)
+
+        self.add_block(AST_return(AST_value(VYPaInt(), self.stack.top())))
 
 
 class ReadStringVYPa(VYPaBuildInFunctionClass):
     def __init__(self):
         super().__init__(VYPaString(), "readString", [])
-        self.function.add_variable(AST_variable(VYPaString(), "string"))
-        self.return_expression = AST_value(VYPaString(), self.stack.top())
-        self.instruction_tape.add(READS(VYPaRegister.DestinationReg))
-        self.stack.set(VYPaRegister.DestinationReg)
+        self.add_block(AST_declaration(VYPaString(), ["string"]))
+
+        reads_block = AST_block()
+        reads_block.add_instruction(READS(VYPaRegister.DestinationReg))
+        reads_block.stack.set(VYPaRegister.DestinationReg)
+        self.add_block(reads_block)
+
+        self.add_block(AST_return(AST_value(VYPaString(), self.stack.top())))
 
 
 class LengthVYPa(VYPaBuildInFunctionClass):
